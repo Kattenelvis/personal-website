6332a4a4d0dce224db0e8ee59fa2b7c436c8ffe6
kattenelvis <53380638+Kattenelvis@users.noreply.github.com>
2025-12-29 20:24:47 +0100

    centered title screen and begun with cellular automata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,9 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <header class="sticky top-0 z-50 w-full">
-	<nav class="flex gap-3 bg-gray-800 p-3 text-white">
+	<nav class="flex justify-center gap-3 bg-gray-800 p-3 text-white">
 		<a class="glassy" href="/">Home</a>
-		<a class="gradient glassy" href="/aero">Fruity!</a>
+		<a class="gradient glassy" href="/aero">Aero!</a>
 		<a class="glassy bg-purple-600" href="/skills">Skills</a>
 		<a class="glassy bg-green-500" href="/contact">Contact</a>
 	</nav>
---
+++ src/routes/cellular/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const X = 10,
+		Y = 10;
+	let grid = new Array(Y).fill(new Array(X).fill(0));
+
+	onMount(() => {
+		let automata: HTMLElement | null = document.querySelector('#automata');
+
+		if (!automata) return;
+
+		automata.style.display = 'grid';
+		automata.style.gridTemplateColumns = 'repeat(10, 1fr)';
+		automata.style.gridTemplateRows = 'repeat(10, 1fr)';
+	});
+</script>
+
+<div id="automata" class="h-screen w-full">
+	{#each grid as y, i}
+		{#each y as x, j}
+			<div class="bg-blue-500"></div>
+		{/each}
+	{/each}
+</div>
+
+<style>
+	/* .automata { */
+	/* 	display: grid; */
+	/* 	grid-column: repeat(1fr, var(--X)); */
+	/* 	grid-row: repeat(1fr, var(--Y)); */
+	/* 	width: 100vw; */
+	/* } */
+</style>
