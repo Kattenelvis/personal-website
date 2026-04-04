9fe0a344ab5ec356eff70be3fbc76ee0848fd290
emil <53380638+Kattenelvis@users.noreply.github.com>
2026-04-04 21:05:46 +0200

    Nicer changes

--- src/routes/academic/+page.svelte
+++ src/routes/academic/+page.svelte
@@ -1,29 +1,76 @@
-<main class="m-auto min-h-screen w-[70%] bg-amber-600 p-6">
-	I've also had an interesting academic career, primarily studying philosophy, logic, computer
-	science and mathematics. I've also spent a lot of time on Stanford Encyclopedia of Philosophy,
-	probably the best source for introductions to philosophical topics I've ever found.
+<script>
+	const padding = 6;
+</script>
 
-	<ul>
-		<li>History of Philosophy I and II</li>
-		<li>Philosophy of Science I and II</li>
-		<li>Logic I and II</li>
-		<li>Philosophical Logic</li>
-		<li>Metalogic</li>
-		<li>Epistemology</li>
-		<li>Philosophy of Mind</li>
-		<li>Mental Content</li>
-		<li>(Un)intelligent design? Arguments for and against the existence of God</li>
-		<li>Philosophy of AI</li>
-		<li>Logic in Computer Science and AI</li>
-		<li>Philosophy of Mathematics</li>
-		<li>Philosophy of Language I and II</li>
-		<li>Experimental Philosophy</li>
-		<li>Philosophy of Physics</li>
-		<li>Mathematics I</li>
-		<li>Linear Algebra</li>
-		<li>Probability Theory I</li>
-		<li>Analysis I</li>
-		<li>Programming Techniques for Mathematicians</li>
-		<li>Computer Science for Mathematicians</li>
-	</ul>
+<main class="m-auto min-h-screen w-[70%] max-w-[1000px]">
+	<div class={`bg-amber-700 p-${padding}`}>
+		<p>
+			I've also had an interesting academic career, primarily studying philosophy, logic, computer
+			science and mathematics. I've also spent a lot of time on Stanford Encyclopedia of Philosophy,
+			probably the best source for introductions to philosophical topics I've ever found.
+		</p>
+	</div>
+
+	<div class={`bg-gray-600 p-${padding}`}>
+		<p>One of my favourite areas of study is Philosophy</p>
+		<ul>
+			<li>History of Philosophy I and II</li>
+			<li>Philosophy of Science I and II</li>
+			<li>Epistemology</li>
+			<li>Philosophy of Mind</li>
+			<li>Mental Content</li>
+			<li>(Un)intelligent design? Arguments for and against the existence of God</li>
+			<li>Philosophy of AI</li>
+			<li>Experimental Philosophy</li>
+			<li>Philosophy of Physics</li>
+			<li>Philosophy of Mathematics</li>
+			<li>Philosophy of Language I and II</li>
+		</ul>
+	</div>
+
+	<div class={`bg-slate-700 p-${padding}`}>
+		<p>Formal Logic and Mathematics.</p>
+		<ul>
+			<li>Logic I and II</li>
+			<li>Philosophical Logic</li>
+			<li>Metalogic</li>
+			<li>Linear Algebra</li>
+			<li>Logic in Computer Science and AI</li>
+			<li>Probability Theory I</li>
+			<li>Mathematics I</li>
+			<li>Analysis I</li>
+		</ul>
+	</div>
+
+	<div class={`bg-green-800 p-${padding}`}>
+		<p>
+			Computer Science. I've always found Universities to not be particularly great with computer
+			science classes. I find it better to make stuff or learn on my own. I plan on taking more
+			advanced CS courses later though, I still find the topic interesting.
+		</p>
+		<ul>
+			<li>Programming Techniques for Mathematicians</li>
+			<li>Computer Science for Mathematicians</li>
+			<li>Web Development</li>
+		</ul>
+	</div>
 </main>
+
+<style>
+	p {
+		padding-bottom: 1rem;
+	}
+
+	li {
+		padding-left: 1rem;
+	}
+
+	main {
+		font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+		color: white;
+	}
+
+	div {
+		margin: 1rem;
+	}
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,5 @@
 <div class="m-auto w-[70%] max-w-[1000px] rounded-md bg-white">
-	<h1>Magnum Opus: Flowback</h1>
+	<h1>Main Project: Flowback</h1>
 
 	My video games in Unity Engine.
 	<img src="/Falling_Down_Game.png" />
